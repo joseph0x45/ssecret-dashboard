b1183fd499473f90f7b33e1f16b2251070e54eee
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-10-22 13:15:38 +0000

    fix: made footer and some ui shits

--- src/components/ProjectBox.svelte
+++ src/components/ProjectBox.svelte
@@ -27,7 +27,12 @@
         <div class="z-10 absolute left-12 bottom-0 w-8 h-8 rounded-full bg-pink-400 border-2 border-pink-800 dark:bg-pink-700 dark:border-pink-500 group-hover:translate-x-3 transition-all" ></div>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <svg on:click={copyID}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="z-50 rounded-full p-1 text-slate-400 hover:bg-slate-300 hover:text-slate-600 dark:text-slate-500 dark:hover:bg-slate-600 dark:hover:text-slate-400 hover:cursor-pointer w-8 h-8 transition-all absolute bottom-4 right-4">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
-    </svg>
+    <div class="has-tooltip absolute bottom-4 right-4" >
+        <span class='tooltip opacity-80 text-[10px] z-50 rounded-lg shadow-lg p-1 dark:bg-slate-900 text-slate-500 mt-10 mr-24 w-12 transition-all'>Copy ID </span>
+        <svg on:click={copyID}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="z-50 rounded-full p-1 text-slate-400 hover:bg-slate-300 hover:text-slate-600 dark:text-slate-500 dark:hover:bg-slate-600 dark:hover:text-slate-400 hover:cursor-pointer w-8 h-8 transition-all ">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+        </svg>
+
+    </div>
+
 </div>
--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts" >
     import Header from "../../components/Header.svelte"
+    import Footer from "../../components/Footer.svelte"
 </script>
 
 
-<div class="fixed w-full h-full bg-slate-100" >
+<div class=" fixed w-full h-full " >
     <Header />
 
     <slot/>
+    <Footer />
 </div>
+
